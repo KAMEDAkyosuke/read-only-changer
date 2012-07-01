9b59597f7cfcd6baa4f65d256e88f47613fd543d
organlounge <organlounge@OrganLoungeMacBookAir.local>
2012-07-02 01:06:18 +0900

    bug fix

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ HOW TO USE
 
     (add-to-list 'load-path "/path/to/your/saved/directory")
     (require 'read-only-changer)
-    (setq read-only-changer-writeable-file-list
-          '("^/path/to/your/writable/directory/or/file"))
+    (setq read-only-changer-ignore-patterns
+          '("^/path/to/your/writable/directory/or/file/patterns"))
     (add-hook 'find-file-hooks 'read-only-changer)
--- read-only-changer.el
+++ read-only-changer.el
@@ -1,19 +1,19 @@
 ;; -*- coding:utf-8; mode:emacs-lisp; lexical-binding: t -*-
-(defvar read-only-changer-writeable-file-list '())
+(defvar read-only-changer-ignore-patterns '())
 
-(defun read-only-changer-writeable-file-p (writeable-file-list)
-  (let ((check-iter (lambda (writeable-file-list)
-                      (cond ((null writeable-file-list)
-                             nil)
-                            ((numberp (string-match (car writeable-file-list)
-                                                    buffer-file-name))
-                             t)
-                            (t
-                             (funcall check-iter (cdr writeable-file-list)))))))
-    (funcall check-iter writeable-file-list)))
+(defun read-only-changer-ignore-p (pattern-list)
+  (letrec ((check-iter (lambda (pattern-list)
+                         (cond ((null pattern-list)
+                                nil)
+                               ((numberp (string-match (car pattern-list)
+                                                       buffer-file-name))
+                                t)
+                               (t
+                                (funcall check-iter (cdr pattern-list)))))))
+    (funcall check-iter pattern-list)))
 
 (defun read-only-changer ()
-  (if (null (read-only-changer-writeable-file-p read-only-changer-writeable-file-list))
+  (if (null (read-only-changer-ignore-p read-only-changer-ignore-patterns))
       (setq buffer-read-only t)))
 
-(provide 'read-only-changer)
+(provide 'read-only-changer
